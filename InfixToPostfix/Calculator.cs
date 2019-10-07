541a6987402d700cf4f55bad5e533c2c63761ed1
Julia <Charly.Fox@mail.ru>
2019-10-07 15:27:22 +0500

    Infix --> Postfix
    TODO: Calculation

---
+++ InfixToPostfix/Calculator.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace InfixToPrefix
+{
+    class Calculator
+    {
+        Dictionary<string, int> priority = new Dictionary<string, int>()
+            { {"(", -1 }, {")",0 },
+            {"+", 1}, {"-", 1},
+            {"*", 2}, {":", 2},
+            { "ln", 3},{"sqrt", 3},{"cos", 3},{"sin", 3},
+            {"^", 4} };
+
+        Dictionary<string, int> variables = new Dictionary<string, int>();
+
+        public double GetResult(string filepath)
+        {
+            string[] input = File.ReadAllLines(filepath);
+            ReadVariables(input);
+            return CalculateInPostfixNotation(InfixToPostfix(input[0]));
+        }
+
+        public double GetResultAndShowProcess(string filepath)
+        {
+            string[] input = File.ReadAllLines(filepath);
+
+            Console.WriteLine("Input expression (infix notation):");
+            Console.WriteLine(input[0]);
+
+            var postfix = InfixToPostfix(input[0]);
+            Console.WriteLine("\nPostfix notation:");
+            PrintExpression(postfix);
+
+            ReadVariables(input);
+            Console.WriteLine("\nVariables found:");
+            PrintVariables();
+
+            var result = CalculateInPostfixNotation(postfix);
+            Console.WriteLine("\nResult:");
+            Console.WriteLine(result);
+            Console.WriteLine("\n\n");
+            return result;
+        }
+
+        public void ReadVariables(string[] input)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                var pair = input[i].Split(new char[] { ' ', '=' }, StringSplitOptions.RemoveEmptyEntries);
+                variables[pair[0]] = int.Parse(pair[1]);
+            }
+        }
+
+        public List<string> InfixToPostfix(string expression)
+        {
+            MyStack functions = new MyStack();
+            string[] words = expression.Split(' '); //разбитое на "слова" выражение
+            List<string> postfix = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (char.IsDigit(word[0]))
+                    postfix.Add(word);
+                else if (word == "(")
+                    functions.Push(word);
+                else if (!priority.ContainsKey(word))
+                    postfix.Add(word);
+                else
+                {
+                    while (!functions.IsEmpty())
+                        if (priority[functions.Top()] >= priority[word])
+                            postfix.Add(functions.Pop());
+                        else break;
+                    if (word == ")")
+                        functions.Pop();
+                    else
+                        functions.Push(word);
+                }
+            }
+            while (!functions.IsEmpty())
+                postfix.Add(functions.Pop());
+
+            return postfix;
+        }
+
+        public double CalculateInPostfixNotation(List<string> expression)
+        {
+            return 0;
+        }
+
+        void PrintExpression(IEnumerable<string> expression)
+        {
+            foreach (var word in expression)
+            {
+                Console.Write(word);
+                Console.Write(' ');
+            }
+            Console.WriteLine();
+        }
+
+        void PrintVariables()
+        {
+            foreach (var pair in variables)
+            {
+                Console.Write("variable: ");
+                Console.Write(pair.Key);
+                Console.Write("\tvalue: ");
+                Console.WriteLine(pair.Value);
+            }
+        }
+    }
+}
---
+++ InfixToPostfix/MyStack.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace InfixToPrefix
+{
+    public class MyStack
+    {
+        List<string> list = new List<string>();
+        public void Push(string value)
+        {
+            list.Add(value);
+        }
+        public string Pop()
+        {
+            if (list.Count == 0) return "Ошибка выполнения команды - Стек пуст";
+            var result = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return result;
+        }
+
+        public string Top()
+        {
+            if (list.Count != 0)
+                return list[list.Count - 1];
+            return "Ошибка выполнения команды - Стек пуст";
+        }
+
+        public bool IsEmpty()
+        {
+            return list.Count == 0;
+        }
+
+        public void Print()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void ExecuteFromFile()
+        {
+            //считывание из файла
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            string input = File.ReadAllText("input.txt");
+
+            watch.Stop();
+            Console.WriteLine("Время считывания: " + watch.Elapsed);
+
+
+            //Выполнение программы
+            watch.Restart();
+
+            var commands = input.Split(' ');
+            var s = new MyStack();
+            for (int i = 0; i < commands.Length; i++)
+            {
+                if (commands[i][0] == '1')
+                {
+                    var val = commands[i].Split(',')[1];
+                    s.Push(val);
+                    Console.WriteLine("Push: " + val);
+                }
+                else if (commands[i] == "2")
+                    Console.WriteLine("Pop: " + s.Pop());
+                else if (commands[i] == "3")
+                    Console.WriteLine("Top: " + s.Top());
+                else if (commands[i] == "4")
+                    Console.WriteLine("IsEmpty: " + s.IsEmpty());
+                else if (commands[i] == "5")
+                {
+                    Console.WriteLine("Print:");
+                    s.Print();
+                }
+            }
+            Console.WriteLine("Время выполнения: " + watch.Elapsed);
+        }
+    }  
+}
